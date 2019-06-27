5fa1bbc96f5a5fd68860ae95ef2ac077a919f16a
Rudra Karmakar <47749203+imrudrakarmakar@users.noreply.github.com>
2019-06-27 23:23:13 +0530

    Create Armstrong number or not.c

---
+++ Basic Program/Armstrong number or not.c
@@ -0,0 +1,25 @@
+//Write a program in C to accept any integer from the user and display whether the number is a Armstrong number or not. 
+
+#include<stdio.h>
+int main()
+{
+	int num,cpnum,arm=0,rem=0;
+	printf("Enter any positie integer: ");
+	scanf("%d",&num);
+	cpnum=num;
+	while(cpnum!=0)
+	{
+		rem=cpnum%10;
+		arm=arm+(rem*rem*rem);
+		cpnum=cpnum/10;
+	}
+	if(num==arm)
+	{
+		printf("%d is Amstrong no",num);
+	}
+	else
+	{
+		printf("%d is not Amstrong no",num);
+	}
+	return 0;
+}
