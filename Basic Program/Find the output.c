2effe51b071dd38ce619cdd66676c7ad99975ac5
Rudra Karmakar <47749203+imrudrakarmakar@users.noreply.github.com>
2019-06-28 00:52:47 +0530

    Create Find the output.c

---
+++ Basic Program/Find the output.c
@@ -0,0 +1,16 @@
+/*find the output of the following program
+  #include<stdio.h>
+int main()
+{
+	int a, i=5;
+	a=i+++i;
+	printf("%d",a);
+}*/
+
+#include<stdio.h>
+int main()
+{
+	int a, i=5;
+	a=i+++i;
+	printf("%d",a);
+}
